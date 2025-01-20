af93425fc8179708322e92aab1c073ccbb7e67fd
Alexandr Kononov <a.kononov@tinkoff.ru>
2025-01-20 15:20:37 +0400

    Final version of rounded button

--- CustomWpfControls/RoundedButton.cs
+++ CustomWpfControls/RoundedButton.cs
@@ -1,20 +1,23 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace CustomWpfControls
 {
-    public class RoundedButton : Button
+    public class RoundedButton
     {
-        public CornerRadius CornerRadius
-        {
-            get => (CornerRadius)GetValue(CornerRadiusProperty);
-            set => SetValue(CornerRadiusProperty, value);
-        }
-
-        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
-            nameof(CornerRadius), 
-            typeof(CornerRadius), 
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
             typeof(RoundedButton),
             new FrameworkPropertyMetadata(new CornerRadius(0d), FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static void SetCornerRadius(UIElement element, CornerRadius value)
+        {
+            element.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static CornerRadius GetCornerRadius(UIElement element)
+        {
+            return (CornerRadius)element.GetValue(CornerRadiusProperty);
+        }
     }
 }
